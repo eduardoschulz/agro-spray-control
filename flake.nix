bc9d1da4bc3dbf7894db904245add4590266ca7d
Eduardo Schulz <eduardo.schulz@protonmail.com>
2025-05-06 11:01:44 -0300

    fix flake

--- flake.nix
+++ flake.nix
@@ -1,51 +1,35 @@
 {
-  description = "Flake Provisorio";
+  description = "Painel de Pulverização - Desenvolvimento";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    # Se quiser usar uma versão estável do Go
-    go.url = "github:golang/go";
-    nodejs.url = "github:nixos/nixpkgs/nixos-24.11#nodejs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, go, nodejs, ... } @ inputs: {
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells = {
+          backend = pkgs.mkShell {
+            name = "backend-shell";
+            packages = with pkgs; [
+              go
+            ];
+          };
 
-    # Ambiente de desenvolvimento para o backend (Go)
-    devShells.x86_64-linux.backend = nixpkgs.mkShell {
-      buildInputs = [
-        go 
-      ];
-      shellHook = ''
-        export SHELL=$(which zsh)
-        export GOPATH=$PWD/go
-        export PATH=$GOPATH/bin:$PATH
-      '';
-    };
-
-    # Ambiente de desenvolvimento para o frontend (Vue.js)
-    devShells.x86_64-linux.frontend = nixpkgs.mkShell {
-      buildInputs = [
-        nodejs # Inclui Node.js para o Vue.js
-      ];
-      shellHook = ''
-        export SHELL=$(which zsh)
-        export PATH=$PWD/node_modules/.bin:$PATH
-      '';
-    };
-
-    # Recomendação para o ambiente de produção
-    packages.x86_64-linux.backend = nixpkgs.buildGoModule {
-      pname = "agrospray-backend";
-      version = "0.1.0";
-      src = ./backend;
-      vendorSha256 = "sha256-hash"; # Adicione o hash dos pacotes de dependências se necessário
-    };
-
-    packages.x86_64-linux.frontend = nodejs.buildPackages.nodePackages.createFrontend {
-      pname = "agrospray-frontend";
-      version = "0.1.0";
-      src = ./frontend;
-    };
-  };
+          frontend = pkgs.mkShell {
+            name = "frontend-shell";
+            packages = with pkgs; [
+              nodejs_20
+              yarn
+            ];
+          };
+        };
+      }
+    );
 }
 
