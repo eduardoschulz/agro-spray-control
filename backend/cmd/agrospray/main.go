1a20893a0cdc45475031e599b0ff8311b29cbcbf
Eduardo Schulz <eduardo.schulz@protonmail.com>
2025-05-06 11:05:16 -0300

    adding main

--- backend/cmd/agrospray/main.go
+++ backend/cmd/agrospray/main.go
@@ -1,6 +1,39 @@
 package main
 
+import (
+
+    "github.com/eduardoschulz/agro-spray-control/backend/pkg/db"
+    "fmt"
+)
+
+type Banco struct {
+    Addr string
+    User string
+    Passwd string
+    Port int16
+    DBName string
+    SSL    string
+}
+
+
 func main(){
 
+
+    /* Isso tem q ser movido para outro lugar posteriormente */
+     b := Banco {
+        Addr: "localhost",
+        User: "postgres",
+        Passwd: "postgres", 
+        Port: 5432,
+        DBName: "go",
+        SSL: "disable",
+    }
+
+
+    connstr := fmt.Sprintf(
+        "host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+        b.Addr, b.Port, b.User, b.Passwd, b.DBName, b.SSL,
+    )
+    db.Connect(connstr)
 
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/eduardoschulz/agro-spray-control/backend
 
 go 1.23.8
+
+require github.com/lib/pq v1.10.9 // indirect
