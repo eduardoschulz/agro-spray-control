985fb5dcc6219c32d6ed8a07ac2df045d298626d
Eduardo Schulz <eduardo.schulz@protonmail.com>
2025-05-08 12:01:34 -0300

    add produtos.go

---
+++ backend/internal/models/produtos.go
@@ -0,0 +1,22 @@
+package models
+
+type Produto struct {
+	Cod             string    `json:"cod" db:"cod"`
+	Descricao string    `json:"descricao" db:"descricao"`
+	Fabricante string    `json:"fabricante" db:"fabricante"`
+	CompQuimica string    `json:"compquimica" db:"compquimica"`
+}
+
+
+func NovoProduto(cod, descricao, fabricante, compquimica string) *Produto {
+
+    
+  p := &Produto{
+    Cod: cod,
+    Descricao: descricao,
+    Fabricante: fabricante,
+    CompQuimica: compquimica,
+}
+  return p
+ }
+
